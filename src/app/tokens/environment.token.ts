bd60aedb510a5968f8dddd8c4695febc71833e0f
Anatolii Pisarivskyi <anatolii.pisarivskyi@globallogic.com>
2021-04-12 13:05:09 +0300

    feat: add env files, add env model, token

---
+++ src/app/tokens/environment.token.ts
@@ -0,0 +1,6 @@
+import { InjectionToken } from '@angular/core';
+
+/**
+ * Token for provide environment
+ */
+export const ENV_TOKEN = new InjectionToken<any>('environment.token');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { EnvironmentInterface } from "src/app/models/environment.model";
+
+export const environment: EnvironmentInterface = {
+  production: true,
+  apiHost: 'http://localhost:3000',
 };
