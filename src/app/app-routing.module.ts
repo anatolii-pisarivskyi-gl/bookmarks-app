30dd2c48b0b4368b8c4266fd01bf2a519a2d7d45
Anatolii Pisarivskyi <anatolii.pisarivskyi@globallogic.com>
2021-04-12 16:23:50 +0300

    improvement: update AppModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainLayoutComponent } from './core/main-layout/main-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'bookmarks',
+      },
+      {
+        path: 'bookmarks',
+        loadChildren: () => import('./features/bookmarks/bookmarks.module').then((m) => m.BookmarksModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutModule } from './core/main-layout/main-layout.module';
+import { environment } from '../environments/environment';
+import { ENV_TOKEN } from './tokens/environment.token';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MainLayoutModule,
+    StoreModule.forRoot({}, {}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ENV_TOKEN,
+      useValue: environment,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
